d371e2e052888f0f3f7d4c9f14a475fd85ca5407
Jonathan Solís Parajeles <jonathan.solis.parajeles@gmail.com>
2015-10-26 11:14:43 -0600

    Add: create_student_page fields. WIP

--- page_objects/create_student_page/create_student_page.py
+++ page_objects/create_student_page/create_student_page.py
@@ -1,14 +1,31 @@
+from selenium.webdriver.common.by import By
+
+
 class CreateStudentLocators:
 
     def __init__(self):
         pass
 
     TITLE = "Expediente Estudiantil - Development Environment"
+    NAME_INPUT = (By.ID, "name")
+    FIRST_LAST_NAME_INPUT = (By.ID, "primer_apellido")
+    SECOND_LAST_NAME_INPUT = (By.ID, "segundo_apellido")
 
-class CreateStudentPage():
+
+
+class CreateStudentPage:
     def __init__(self, driver):
         self.driver = driver
 
     def is_title_displayed(self):
         return self.driver.wait_for_title_contains(CreateStudentLocators.TITLE, 30)
+
+    def enter_name(self, name):
+        self.driver.send_keys(CreateStudentLocators.NAME_INPUT[0], CreateStudentLocators.NAME_INPUT[1], name)
+
+    def enter_first_last_name(self, first_last_name):
+        self.driver.send_keys(CreateStudentLocators.FIRST_LAST_NAME_INPUT[0], CreateStudentLocators.FIRST_LAST_NAME_INPUT[1], first_last_name)
+
+    def enter_second_last_name(self, second_last_name):
+        self.driver.send_keys(CreateStudentLocators.SECOND_LAST_NAME_INPUT[0], CreateStudentLocators.SECOND_LAST_NAME_INPUT[1], second_last_name)
 
--- tests/create_student_test_suite.py
+++ tests/create_student_test_suite.py
@@ -3,9 +3,12 @@ from base.base_test import BaseTest
 from page_objects.create_student_page.create_student_page import CreateStudentPage
 from configs.base_framework_configs import GlobalConfigs as GC
 from configs.base_framework_configs import GlobalConfigsMessages as GM
+import time
 
 class CreateStudentTestSuite(BaseTest):
 
     def test_layout(self):
         create_student_page = CreateStudentPage(self.driver)
         self.assertTrue(create_student_page.is_title_displayed(), GM.ERROR_CREATE_STUDENT_PAGE)
+        create_student_page.enter_name("Hola mundo")
+        time.sleep(5)
