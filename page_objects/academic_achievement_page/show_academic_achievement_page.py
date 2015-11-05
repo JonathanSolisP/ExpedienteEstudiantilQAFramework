1cb168cb9d6c67f289d42ffc7873121a7e62563c
Lesmed Gutierrez <lesmedgutiar.16@gmail.com>
2015-11-05 10:25:25 -0600

    Add: Delete academic achievements test

---
+++ page_objects/academic_achievement_page/show_academic_achievement_page.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.common.by import By
+
+__author__ = 'Proyecto'
+
+
+class ShowAcademicAchievementsLocators:
+
+    def __init__(self):
+        pass
+
+    TITLE = "Expediente Estudiantil - Development Environment"
+    DELETE_ACADEMIC_ACHIEVEMENT_BUTTON = (By.XPATH, "/html/body/section/section/section/section/div[2]/a[2]")
+    HEADER_ACADEMIC_ACHIEVEMENT_BUTTON = (By.XPATH, "/html/body/section/section/section/section/div[1]/h1")
+
+
+
+class ShowAcademicAchievementPage:
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click_delete_academic_achievement_button(self):
+        self.driver.click(*ShowAcademicAchievementsLocators.DELETE_ACADEMIC_ACHIEVEMENT_BUTTON)
+
+    def is_header_show_displayed(self):
+        return self.driver.wait_for_element_visible(ShowAcademicAchievementsLocators.HEADER_ACADEMIC_ACHIEVEMENT_BUTTON[0],
+                                                    ShowAcademicAchievementsLocators.HEADER_ACADEMIC_ACHIEVEMENT_BUTTON[1], 30)
--- tests/academic_achievement_of_student_page_test_suite.py
+++ tests/academic_achievement_of_student_page_test_suite.py
@@ -1,15 +1,11 @@
 from base.base_test import BaseTest
 
 from page_objects.academic_achievement_page.academic_achievement_page import AcademicAchievementPage
-from page_objects.create_student_page.create_student_page import CreateStudentPage
+from page_objects.academic_achievement_page.show_academic_achievement_page import ShowAcademicAchievementPage
 from page_objects.home_page.home_page import HomePage
 from page_objects.login_page.login_page import LoginPage
 from page_objects.view_student_page.view_student_page import ViewStudentPage
 from configs.base_framework_configs import GlobalConfigs as GC
-from configs.base_framework_configs import GlobalConfigsMessages as GM
-import time
-
-
 
 class AddAcademicAchievementStudent(BaseTest):
 
@@ -21,8 +17,6 @@ class AddAcademicAchievementStudent(BaseTest):
         login_page.enter_password(GC.PASSWORD)
         login_page.click_sign_in_button()
         home_page.is_user_name_displayed()
-        ###home_page.click_student_tab()
-        ###home_page.select_create_student_option()
 
     def test_add_academic_achievement_successful(self):
         self.go_to_page()
@@ -35,7 +29,6 @@ class AddAcademicAchievementStudent(BaseTest):
         create_academic_achievement_page.click_academic_achievement_button()
         viewStudentPage = ViewStudentPage(self.driver)
         viewStudentPage.is_add_responsible_button_displayed()
-        time.sleep(5)
 
     def test_add_academic_achievement_empty_fields(self):
         self.go_to_page()
@@ -46,4 +39,13 @@ class AddAcademicAchievementStudent(BaseTest):
         create_academic_achievement_page.enter_description("Juegos")
         create_academic_achievement_page.click_academic_achievement_button()
         create_academic_achievement_page.is_error_message_displayed()
-        time.sleep(5)
+
+    def test_delete_academic_achievement_successfull(self):
+        self.go_to_page()
+        my_url = "http://172.24.28.21:3000/#!/logros-academicos/56392245bf6ad92638c8a0d6//563b7feebf6ad92638c8a1cf"
+        self.driver.get(my_url)
+        delete_academic_achievement_page = ShowAcademicAchievementPage(self.driver)
+        delete_academic_achievement_page.click_delete_academic_achievement_button()
+        delete_academic_achievement_page.is_header_show_displayed()
+
+
