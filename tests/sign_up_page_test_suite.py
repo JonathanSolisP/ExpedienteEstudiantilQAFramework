8cb1fcd8ba5092d611e5f07f4e5c2337b34596fd
Randall Gonzalez <randy1002@gmail.com>
2015-11-05 15:56:46 -0600

    test: Sign up test suite complete.

--- tests/sign_up_page_test_suite.py
+++ tests/sign_up_page_test_suite.py
@@ -36,10 +36,33 @@ class SignUpPageTest(BaseTest):
     def test_sign_up_existing_user(self):
         self.go_to_sign_up_page()
         sign_up_page = SignUpPage(self.driver)
-        sign_up_page.enter_first_name("Josue")
+        sign_up_page.enter_first_name("Andres")
         sign_up_page.enter_last_name("Morales")
-        sign_up_page.enter_email("jocampos@gmail.com")
+        sign_up_page.enter_email("andresmorales@gmail.com")
         sign_up_page.enter_username("jocampos")
         sign_up_page.enter_password("jocam1994")
         sign_up_page.click_sign_up_button()
         self.assertTrue(sign_up_page.is_sign_up_error_message_display(), GM.MISSING_ERROR_MSG)
+
+
+    def test_sign_up_invalid_email(self):
+        self.go_to_sign_up_page()
+        sign_up_page = SignUpPage(self.driver)
+        sign_up_page.enter_first_name("Josue")
+        sign_up_page.enter_last_name("Morales")
+        sign_up_page.enter_email("jocampos.com")
+        sign_up_page.click_sign_up_button()
+        self.assertTrue(sign_up_page.is_sign_up_error_message_display(), GM.MISSING_ERROR_MSG)
+
+
+    def test_sign_up_short_password(self):
+        self.go_to_sign_up_page()
+        sign_up_page = SignUpPage(self.driver)
+        sign_up_page.enter_first_name("Josue")
+        sign_up_page.enter_last_name("Morales")
+        sign_up_page.enter_email("jocampos.com")
+        sign_up_page.enter_username("jocampos")
+        sign_up_page.enter_password("1")
+        sign_up_page.click_sign_up_button()
+        self.assertTrue(sign_up_page.is_sign_up_error_message_display(), GM.MISSING_ERROR_MSG)
+
