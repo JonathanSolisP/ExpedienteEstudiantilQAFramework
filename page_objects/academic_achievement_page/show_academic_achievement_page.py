e9d5b21863a9bed14ecaa17489515dd70d99e57a
Lesmed Gutierrez <lesmedgutiar.16@gmail.com>
2015-11-06 11:12:26 -0600

    Add: update academic achievement

--- page_objects/academic_achievement_page/show_academic_achievement_page.py
+++ page_objects/academic_achievement_page/show_academic_achievement_page.py
@@ -11,6 +11,8 @@ class ShowAcademicAchievementsLocators:
     TITLE = "Expediente Estudiantil - Development Environment"
     DELETE_ACADEMIC_ACHIEVEMENT_BUTTON = (By.XPATH, "/html/body/section/section/section/section/div[2]/a[2]")
     HEADER_ACADEMIC_ACHIEVEMENT_BUTTON = (By.XPATH, "/html/body/section/section/section/section/div[1]/h1")
+    UPDATE_ACADEMIC_ACHIEVEMENT_BUTTON = (By.XPATH, "/html/body/section/section/section/section/div[2]/form/fieldset/div[2]/input")
+
 
 
 
@@ -20,6 +22,9 @@ class ShowAcademicAchievementPage:
 
     def click_delete_academic_achievement_button(self):
         self.driver.click(*ShowAcademicAchievementsLocators.DELETE_ACADEMIC_ACHIEVEMENT_BUTTON)
+
+    def click_update_academic_achievement_button(self):
+        self.driver.click(*ShowAcademicAchievementsLocators.UPDATE_ACADEMIC_ACHIEVEMENT_BUTTON)
 
     def is_header_show_displayed(self):
         return self.driver.wait_for_element_visible(ShowAcademicAchievementsLocators.HEADER_ACADEMIC_ACHIEVEMENT_BUTTON[0],
